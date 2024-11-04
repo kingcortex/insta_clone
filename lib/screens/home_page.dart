f4f9599dfba69e34b3345b631aa2e2810a99da1f
Astroworld <souleydiom225@gmail.com>
2024-11-04 23:09:25 +0000

    _

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
 import 'package:instagram_clone/data/storie_model.dart';
